85c18ef66b81a8db575768eca1c22c577bfd7375
Hexcrow-k17 <kimaleksander1702@gmail.com>
2023-10-20 13:26:36 +0200

    // Get references to the input box and list container elements#23

---
+++ To-Do-List/JavaScript/script.js
@@ -0,0 +1,4 @@
+// Get references to the input box and list container elements
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
