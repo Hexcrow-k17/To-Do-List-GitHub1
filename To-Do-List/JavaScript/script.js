0a46791a471e1ce74f0d9a5732b1fda09a70cbf2
Hexcrow-k17 <kimaleksander1702@gmail.com>
2023-10-20 13:27:25 +0200

    // Event listener for the input box #24

--- To-Do-List/JavaScript/script.js
+++ To-Do-List/JavaScript/script.js
@@ -2,3 +2,11 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+// Event listener for the input box
+inputBox.addEventListener('keyup', function (event) {
+    // Check if the enter key was pressed
+    if (event.keyCode === 13) {
+        // If enter key was pressed add a new task
+        addTask();
+    }
+});
